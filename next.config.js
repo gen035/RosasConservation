6d5d9f578ba20903c241f523d163c6e2b9dd92e9
Genevieve Perron Migneron <info@gen-migneron.com>
2022-08-12 13:43:03 -0400

    Style content

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 const nextTranslate = require('next-translate');
-const withSvgr = require("next-svgr");
 module.exports = {
-  ... nextTranslate(),
-  ...withSvgr(),
-  trailingSlash: false,
+  ... nextTranslate()
 }
