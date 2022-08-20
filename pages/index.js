d22374fc60c6b085ac163d4821da22549fda7592
Genevieve Perron Migneron <info@gen-migneron.com>
2022-08-20 00:39:49 -0400

    Fix OG image

--- pages/index.js
+++ pages/index.js
@@ -31,12 +31,12 @@ export default function Home() {
         <meta property="og:url" content="https://www.rosasconservation.com" />
         <meta property="og:title" content={`${t('home:title')} - ${t('home:description')}`} />
         <meta property="og:description" content={t('home:description')} />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="/og.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.rosasconservation.com" />
         <meta property="twitter:title" content={`${t('home:title')} - ${t('home:description')}`} />
         <meta property="twitter:description" content={t('home:description')} />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+        <meta property="twitter:image" content="/og.jpg"></meta>
         {/**
          * Favicons
          */}
