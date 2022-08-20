e2473ed987702f25838994cc76474101ba85af79
Genevieve Perron Migneron <info@gen-migneron.com>
2022-08-19 23:27:16 -0400

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'
 import '../styles/globals.scss'
 import AppContext from "../AppContext";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   const [context, setContext] = useState({
     isContactModalOpened: false
   });
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <AppContext.Provider value={[context, setContext]}>
